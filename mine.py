5abbf3f22695399f6767707f1528fc478f3d7671
Msteryman <123405107+Msteryman@users.noreply.github.com>
2024-02-21 00:01:37 +0400

    abb line function

--- mine.py
+++ mine.py
@@ -7,7 +7,8 @@ def face_capture():
     
     clf = cv2.CascadeClassifier(cascade_path)
     camera = cv2.VideoCapture(0)
-
+    x_2 = 0
+    y_2 = 0
     while True:
         _, frame = camera.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -19,16 +20,20 @@ def face_capture():
             minSize=(30, 30),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
+    
         
         for (x, y, width, height) in faces:
-            cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
+            x_2 = x + width
+            y_2 = y + height
+            cv2.rectangle(frame, (x, y), (x_2, y_2), (255, 255, 0), 2)
+            cv2.circle(frame, (320,240),  2, (255, 255, 0), 2)
+            cv2.line(frame, (320,240), (int((x + x_2) / 2), int((y + y_2) / 2)), (255, 255, 0), 2)
             
-            print(x,y)
             
         cv2.imshow('Faces', frame)
-
         if cv2.waitKey(1) == ord('q'):
             break
+        
         
     camera.release()
     cv2.destroyAllWindows()
