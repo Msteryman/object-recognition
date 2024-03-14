8b8d13b7a5872a33fc607175bb912b5930d24134
Msteryman <123405107+Msteryman@users.noreply.github.com>
2024-03-14 18:31:54 +0400

    abb

--- motor_driver.py
+++ motor_driver.py
@@ -1,9 +1,21 @@
 import RPi.GPIO as GPIO
+from pynput import keyboard
 import time
-
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
-while True:
-    GPIO.output(7,1)
-    time.sleep(1)
-GPIO.cleanup()
+GPIO.setup(7, GPIO.OUT)
+GPIO.output(7, GPIO.HIGH)
+
+
+def clean_up():
+    GPIO.cleanup()
+
+def on_press(key):
+    try:
+        if key.char == 't':
+            clean_up()
+            return False  
+    except AttributeError:
+        pass
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
