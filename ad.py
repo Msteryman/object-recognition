1f3bd44d08a96aa8772fe106cdb1f30e83e52a97
Msteryman <123405107+Msteryman@users.noreply.github.com>
2024-03-20 23:28:15 +0400

    a

--- ad.py
+++
@@ -1,9 +0,0 @@
-import keyboard
-
-def say_hello():
-    print("Привет, мир!")
-
-keyboard.add_hotkey('t', say_hello)
-
-keyboard.wait('esc')  # Программа будет работать до нажатия клавиши "esc" для выхода
-print('hell')
--- mine.py
+++ mine.py
@@ -1,9 +1,9 @@
 import cv2
 import math
-
+import keyboard
 
 def face_capture():
-    cascade_path = r'C:\Users\user\Desktop\object-recognition\haarcascade_frontalface_alt2.xml'
+    cascade_path = r'/home/user/object-recognition/haarcascade_frontalface_alt2.xml'
     
     clf = cv2.CascadeClassifier(cascade_path)
     camera = cv2.VideoCapture(0)
@@ -25,16 +25,17 @@ def face_capture():
         for (x, y, width, height) in faces:
             x_2 = x + width
             y_2 = y + height
-            cv2.rectangle(frame, (x, y), (x_2, y_2), (255, 255, 0), 2)
-            cv2.circle(frame, (320,240),  2, (255, 255, 0), 2)
             canter_x = int((x + x_2) / 2)
             canter_y = int((y + y_2) / 2)
 
-            cv2.line(frame, (320,240), (canter_x, canter_y), (255, 255, 0), 2)
             
-            cv2.line(frame,  ((canter_x), 240), (320,240), (255, 255, 0), 2)
-            cv2.line(frame,  (320,240), (320,canter_y), (255, 255, 0), 2)
+            cv2.line(frame,  ((canter_x), 240), (320,240), (255, 255, 0), 2) # Линия для x к центру x
+            cv2.line(frame,  (320,240), (320,canter_y), (255, 255, 0), 2) # Линия для y к центру y
 
+            distances_x = math.sqrt((canter_x-320)**2 + (240-240)**2)
+            distances_y = math.sqrt((320-320)**2 + (240-canter_y)**2)
+            print(f" x distances is {distances_x}")
+            print(f" y distances is {distances_y}")
             hypotenuse_length = math.sqrt((320 - canter_x)**2 + (240 - canter_y)**2)
             leg_length = math.sqrt((240-canter_y)**2)
             try:
@@ -42,14 +43,14 @@ def face_capture():
             except ZeroDivisionError:
                 triangle_sin = 0
             triangle_corner = math.degrees(math.asin(triangle_sin))
-            print(triangle_corner)
-        cv2.imshow('Faces', frame)
-        if cv2.waitKey(1) == ord('q'):
+            
+        
+        if keyboard.is_pressed('q'):
             break
         
         
     camera.release()
-    cv2.destroyAllWindows()
+    
 
 
 def main():
